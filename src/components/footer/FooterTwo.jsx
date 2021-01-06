0d99ae2e695f8e121a807f010b4d7d0ee1c5518e
nikkig124 <nikkig124@gmail.com>
2021-01-06 09:24:13 -0600

    remove unused css, fix footer

--- src/components/footer/FooterTwo.jsx
+++ src/components/footer/FooterTwo.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import Image from 'gatsby-image';
-import { graphql, useStaticQuery } from "gatsby"
-import {FaTwitter ,FaInstagram ,FaFacebookF , FaLinkedinIn} from "react-icons/fa";
+import React from "react";
+import Image from "gatsby-image";
+import { graphql, useStaticQuery } from "gatsby";
+import styled from "styled-components";
 
-const SocialShare = [
-    {Social: <FaFacebookF /> , link: 'https://www.facebook.com/'},
-    {Social: <FaLinkedinIn /> , link: 'https://www.linkedin.com/'},
-    {Social: <FaInstagram /> , link: 'https://www.instagram.com/'},
-    {Social: <FaTwitter /> , link: 'https://twitter.com/'},
-]
+import {
+  FaTwitter,
+  FaInstagram,
+  FaFacebookF,
+  FaLinkedinIn,
+} from "react-icons/fa";
+
+// const SocialShare = [
+//   { Social: <FaFacebookF />, link: "https://www.facebook.com/" },
+//   { Social: <FaLinkedinIn />, link: "https://www.linkedin.com/" },
+//   { Social: <FaInstagram />, link: "https://www.instagram.com/" },
+//   { Social: <FaTwitter />, link: "https://twitter.com/" },
+// ];
 
 // const query = graphql`
 //   {
@@ -23,41 +30,57 @@ const SocialShare = [
 // `
 
 const FooterTwo = () => {
-    // const {
-    //     file: {
-    //       img: { fluid },
-    //     },
-    //   } = useStaticQuery(query)
+  // const {
+  //     file: {
+  //       img: { fluid },
+  //     },
+  //   } = useStaticQuery(query)
 
-    return (
-        <div className="footer-style-2 ptb--30 bg_image bg_image--1" data-black-overlay="6">
-            <div className="wrapper plr--50 plr_sm--20">
-                <div className="row align-items-center justify-content-between">
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div className="inner">
-                            <div className="logo text-center text-sm-left mb_sm--20">
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div className="inner text-center">
-                            <ul className="social-share rn-lg-size d-flex justify-content-center liststyle">
-                                {SocialShare.map((val , i) => (
-                                    <li key={i}><a href={`${val.link}`}>{val.Social}</a></li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-12 col-sm-12 col-12">
-                        <div className="inner text-lg-right text-center mt_md--20 mt_sm--20">
-                            <div className="text">
-                                <p>Copyright © 2020 Rainbow-Themes. All Rights Reserved.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <Wrapper classname="section">
+      <div
+        className="footer-style-2 ptb--30 bg_image bg_image--1"
+        data-black-overlay="6"
+      >
+        <div className="wrapper plr--50 plr_sm--20">
+          <div className="row align-items-center justify-content-between">
+            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
+              <div className="inner">
+                <div className="logo text-center text-sm-left mb_sm--20"></div>
+              </div>
             </div>
+            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
+              <div className="inner text-center">
+                {/* <ul className="social-share rn-lg-size d-flex justify-content-center liststyle">
+                {SocialShare.map((val, i) => (
+                  <li key={i}>
+                    <a href={`${val.link}`}>{val.Social}</a>
+                  </li>
+                ))}
+              </ul> */}
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+              <div className="inner text-lg-center text-center mt_md--20 mt_sm--20">
+                <div className="text">
+                  <p>Copyright © {(new Date().getFullYear())} Carolyn Shetter. All Rights Reserved.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .footer-style-2 {
+    p {
+      color: #c6c9d8;
+      font-size: 14px;
+      opacity: 0.75;
+    }
+  }
+`;
 export default FooterTwo;
